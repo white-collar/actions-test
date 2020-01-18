d55864ef770ee2e992808bb014a910065b33bd98
Eugene Shmorgun <d-shersh@mail.ru>
2020-01-18 23:44:23 +0200

    actions test commit

---
+++ src/main/java/com/alevel/exceptions/ExceptionsApplication.java
@@ -0,0 +1,13 @@
+package com.alevel.exceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionsApplication.class, args);
+	}
+
+}
